c36b0d683bc4320ba9ceefab9bbde7e3ff0c8dbf
Shroominic <db2002dominic@gmail.com>
2023-12-17 16:06:26 +0100

    ✨ Add chat UI endpoint

---
+++ src/fastui_chat/chat.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter
+from fastui import AnyComponent, FastUI
+from fastui import components as c
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory.buffer import ConversationBufferMemory
+
+router = APIRouter()
+
+memory = ConversationBufferMemory()
+chat = ConversationChain(llm=ChatOpenAI(), memory=memory)
+
+memory.chat_memory.add_ai_message("How can I help you today?")
+
+
+@router.get("/api/", response_model=FastUI, response_model_exclude_none=True)
+async def chat_ui(user_msg: str | None = None) -> list[AnyComponent]:
+    """
+    Show a table of users, `/api` is the endpoint the frontend will connect to
+    when a user fixes `/` to fetch components to render.
+    """
+    if user_msg:
+        await chat.apredict(input=user_msg)
+    return [
+        c.Page(
+            components=[
+                *(
+                    c.Div(
+                        components=[
+                            c.Heading(
+                                text=("You" if msg.type != "ai" else "ChatBot"),
+                                level=6,
+                            ),
+                            c.Text(text=msg.content),
+                        ],
+                        class_name="container col-sm-4 my-4",
+                    )
+                    for msg in memory.chat_memory.messages
+                ),
+                c.Form(
+                    submit_url=".",
+                    method="GOTO",
+                    form_fields=[
+                        c.FormFieldInput(
+                            title="",
+                            name="user_msg",
+                            placeholder="Message ChatBot...",
+                            class_name="py-4",
+                        ),
+                    ],
+                    footer=[],
+                ),
+            ],
+        )
+    ]
