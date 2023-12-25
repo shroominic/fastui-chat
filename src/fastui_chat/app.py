48a247659704e3612e6c487c1bf06d40e88ff925
Shroominic <db2002dominic@gmail.com>
2023-12-25 17:33:00 +0100

    ✨ Refactor app initialization structure

--- src/fastui_chat/app.py
+++ src/fastui_chat/app.py
@@ -1,26 +1,32 @@
+from typing import Any
+
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastui import prebuilt_html
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables import Runnable
 
 from .chat import router as chat_router
-
-app = FastAPI()
-app.include_router(chat_router, prefix="/api")
-
-
-@app.get("/{path:path}", response_class=HTMLResponse)
-async def html_landing() -> HTMLResponse:
-    """
-    Simple HTML page which serves the React app, comes last as it matches all paths.
-    """
-    return HTMLResponse(prebuilt_html(title="ChatBot"))
+from .db import init_database
+from .fastui_core import router as core_router
 
 
-def start() -> None:
-    import uvicorn
+class ChatUI(FastAPI):
+    def __init__(
+        self,
+        chat_history: BaseChatMessageHistory,
+        chat_handler: Runnable,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        init_database(chat_history, chat_handler)
+        self.include_router(chat_router, prefix="/api")
+        self.include_router(core_router)
 
-    uvicorn.run("fastui_chat.app:app", reload=True)
-
-
-if __name__ == "__main__":
-    start()
+    def start_with_uvicorn(self) -> None:
+        try:
+            import uvicorn
+        except ImportError:
+            print("Please install uvicorn with `pip install uvicorn`")
+            exit(1)
+        else:
+            uvicorn.run(self)
