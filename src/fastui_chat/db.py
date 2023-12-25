3fffc7a6dc62b4b8ceef7a5ff281a9ce48a3e505
Shroominic <db2002dominic@gmail.com>
2023-12-25 17:33:00 +0100

    🚀 Add chat database setup

---
+++ src/fastui_chat/db.py
@@ -0,0 +1,40 @@
+from typing import AsyncGenerator, TypedDict
+
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables import Runnable
+
+from .session import ChatSession
+
+
+class FakeDatabase(TypedDict, total=False):
+    chat_history: BaseChatMessageHistory
+    chat_handler: Runnable
+
+
+database: FakeDatabase = {}
+
+
+def init_database(
+    chat_history: BaseChatMessageHistory,
+    chat_handler: Runnable,
+) -> None:
+    database["chat_history"] = chat_history
+    database["chat_handler"] = chat_handler
+
+
+# FastAPI Dependencies:
+
+
+async def get_history() -> AsyncGenerator[BaseChatMessageHistory, None]:
+    if "chat_history" not in database:
+        raise RuntimeError("Database not initialized")
+    yield database["chat_history"]
+
+
+async def get_session() -> AsyncGenerator[ChatSession, None]:
+    if "chat_history" not in database or "chat_handler" not in database:
+        raise RuntimeError("Database not initialized")
+    yield ChatSession(
+        history=database["chat_history"],
+        chat_handler=database["chat_handler"],
+    )
