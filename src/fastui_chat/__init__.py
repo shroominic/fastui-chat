a762f5c1c3666ceb4e589452e0f8b664d2e5e8c2
Shroominic <db2002dominic@gmail.com>
2023-12-25 17:33:00 +0100

    ✨ Add chat UI components

--- src/fastui_chat/__init__.py
+++ src/fastui_chat/__init__.py
@@ -1,1 +1,10 @@
-# TODO: ChatAPP (Ready to use with uvicorn) + ChatComponent (FastUI Component)
+from .app import ChatUI
+from .session import basic_chat_handler
+
+__all__ = [
+    "ChatUI",
+    "basic_chat_handler",
+]
+
+# MaybeTODO: Indipendent ChatComponent (FastUI Component)
+# to be used by eg Flask or Django or other frameworks
