ad64c2f9298affa8c8f845f348878042b34c6ea9
Shroominic <db2002dominic@gmail.com>
2024-02-07 23:12:49 +0800

    🔄 Refactor chat handler setup

--- examples/fastapi_router.py
+++ examples/fastapi_router.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastui_chat.chat import ChatAPIRouter
-from fastui_chat.history import InMemoryChatMessageHistory, init_history_callable
 from fastui_chat.runtime import router as fastui_runtime
-from fastui_chat.session import basic_chat_handler
-from langchain_openai.chat_models import ChatOpenAI
+from funcchain.syntax.components.handler import create_chat_handler
+from funcchain.utils.memory import InMemoryChatMessageHistory, create_history_factory
 
 app = FastAPI()
 
-history_getter = init_history_callable(InMemoryChatMessageHistory)
-chat_handler = basic_chat_handler(llm=ChatOpenAI(), history_getter=history_getter)
+history_getter = create_history_factory(InMemoryChatMessageHistory)
+chat_handler = create_chat_handler(llm="gpt-4", history_getter=history_getter)
 
 app.include_router(
     ChatAPIRouter(history_getter, chat_handler),
