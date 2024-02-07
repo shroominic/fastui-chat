0bd37d7b56828fae68d4e4ea3d3abd9eecd631f6
Shroominic <db2002dominic@gmail.com>
2024-02-07 23:12:49 +0800

    🔄 Refactor chat session implementation

--- src/fastui_chat/session.py
+++ src/fastui_chat/session.py
@@ -1,66 +1,48 @@
-from typing import Annotated, AsyncGenerator, Callable
+from typing import Annotated, AsyncGenerator, AsyncIterator, Callable
 
 from fastapi import Path
-from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-
-from .history import InMemoryChatMessageHistory, init_history_callable
-from .types import ChatHandler, HistoryGetter
+from funcchain.schema.types import ChatHandler, ChatHistoryFactory
+from langchain_core.messages import AIMessage, AIMessageChunk, HumanMessage
+from langchain_core.runnables import RunnableConfig
 
 
-class ChatSession:
+class ChatSession:  # RunnableBindingBase?
     def __init__(
         self,
         *,
         session_id: str,
         chat_handler: ChatHandler,
-        history_getter: HistoryGetter,
+        history_getter: ChatHistoryFactory,
     ) -> None:
         self.session_id = session_id
         self.history = history_getter(session_id)
         self.chat_handler = chat_handler  # maybe make this chat_handler_getter
 
-    async def astream(self, user_msg: str):
-        async for message in self.chat_handler.astream(
-            HumanMessage(content=user_msg),
-            config={
-                "run_name": "ChatMessage",
-                "configurable": {"session_id": self.session_id},
+    @property
+    def config(self) -> RunnableConfig:
+        return {
+            "run_name": "ChatMessage",
+            "configurable": {
+                "session_id": self.session_id,
             },
-        ):
-            yield message
+        }
 
+    def invoke(self, msg: HumanMessage) -> AIMessage:
+        return self.chat_handler.invoke(msg, self.config)
 
-def basic_chat_handler(
-    llm: BaseChatModel,
-    history_getter: HistoryGetter | None = None,
-    system_message: str = "",
-) -> ChatHandler:
-    history_getter = history_getter or init_history_callable(InMemoryChatMessageHistory)
-    handler = (
-        ChatPromptTemplate.from_messages(
-            [
-                *(("system", system_message) if system_message else []),
-                MessagesPlaceholder(variable_name="history"),
-                ("human", "{user_msg}"),
-            ]
-        )
-        | llm
-    )
-    return {
-        "user_msg": lambda x: x.content,
-    } | RunnableWithMessageHistory(
-        handler,
-        get_session_history=history_getter,
-        input_messages_key="user_msg",
-        history_messages_key="history",
-    )
+    async def ainvoke(self, msg: HumanMessage) -> AIMessage:
+        return await self.chat_handler.ainvoke(msg, self.config)
+
+    def stream(self, msg: HumanMessage) -> AsyncIterator[AIMessageChunk]:
+        yield from self.chat_handler.stream(msg, self.config)
+
+    async def astream(self, msg: HumanMessage) -> AsyncIterator[AIMessageChunk]:
+        async for chunk in self.chat_handler.astream(msg, self.config):
+            yield chunk
 
 
 def create_get_chat_session_dependency(
-    history_getter: HistoryGetter,
+    history_getter: ChatHistoryFactory,
     chat_handler: ChatHandler,
 ) -> Callable[[str], AsyncGenerator[ChatSession, None]]:
     """
