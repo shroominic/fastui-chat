fbf1cbead9019c6dcb1687aefd4d2e780c696344
Shroominic <db2002dominic@gmail.com>
2024-02-07 12:10:14 +0700

    🔧 Simplify chatbot setup

--- examples/simple_chatbot.py
+++ examples/simple_chatbot.py
@@ -1,19 +1,8 @@
 from fastui_chat import ChatUI, basic_chat_handler
-from funcchain.utils.memory import ChatMessageHistory
 from langchain_openai.chat_models import ChatOpenAI
 
-history = ChatMessageHistory()
+handler = basic_chat_handler(llm=ChatOpenAI())
 
-handler = basic_chat_handler(
-    llm=ChatOpenAI(),
-    get_session_history=lambda session_id: history,
-)
-
-history.add_ai_message("How can I help you today?")
-
-app = ChatUI(
-    chat_history=history,
-    chat_handler=handler,
-)
+app = ChatUI(chat_handler=handler)
 
 app.start_with_uvicorn()
