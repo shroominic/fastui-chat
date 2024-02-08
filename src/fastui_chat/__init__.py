888d3e51eed5bd6d794ff50888bfecbdfb859556
Shroominic <db2002dominic@gmail.com>
2024-02-08 09:34:32 +0800

    ✨ Add chat handler utilities

--- src/fastui_chat/__init__.py
+++ src/fastui_chat/__init__.py
@@ -1,10 +1,18 @@
+from funcchain.schema.types import ChatHandler, ChatHistoryFactory
+from funcchain.syntax.components.handler import create_chat_handler
 from funcchain.syntax.components.handler import (
     create_chat_handler as basic_chat_handler,
 )
+from funcchain.utils.memory import InMemoryChatMessageHistory, create_history_factory
 
 from .app import ChatUI
 
 __all__ = [
     "ChatUI",
     "basic_chat_handler",
+    "create_chat_handler",
+    "create_history_factory",
+    "ChatHandler",
+    "ChatHistoryFactory",
+    "InMemoryChatMessageHistory",
 ]
